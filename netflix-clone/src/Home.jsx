a1253a61b2bfa5e6eeb4bb22ae15b85e5ee56430
Neftalem <dneftalem@gmail.com>
2025-07-18 15:07:01 +0300

    creat new div and nav &style

--- netflix-clone/src/Home.jsx
+++ netflix-clone/src/Home.jsx
@@ -1,4 +1,5 @@
 import "./Home.css"
+import logopic from "./assets/netflix-img/icons8-netflix-48.png"
 
 function Home() {
 
@@ -7,6 +8,26 @@ function Home() {
         <div className="home_container">
 
             <div className="display_container">
+
+                <div className="nav_bar_container">
+
+                    <nav>
+
+                        <div className="logo">
+
+                            <img src={logopic} alt="Netflix-logo" />
+
+                               <a href="">Home</a>
+                               <a href="">Home</a>
+                               <a href="">Home</a>
+                               <a href="">Home</a>
+                               <a href="">Home</a>
+
+                        </div>
+
+                    </nav>
+
+                </div>
 
             </div>
 
